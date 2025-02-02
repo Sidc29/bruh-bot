f09ed9a0b406d7c46315344066d5b269d4037687
Shiddharth Choudhari <sidc29@protonmail.com>
2025-02-02 15:42:04 +0530

    Add handleTestChatbot prop to ChatbotIntegration component

--- src/components/registration/steps/ChatbotIntegration/ChatbotIntegration.jsx
+++ src/components/registration/steps/ChatbotIntegration/ChatbotIntegration.jsx
@@ -78,6 +78,7 @@ export const ChatbotIntegration = () => {
             handleSocialShare={handleSocialShare}
             handleAdminPanelView={handleAdminPanelView}
             handleGoBackToMain={handleGoBackToMain}
+            handleTestChatbot={handleTestChatbot}
           />
         ) : showIntegrationSteps ? (
           <IntegrationOptions
